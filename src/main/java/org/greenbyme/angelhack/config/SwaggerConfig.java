698d3cfdffb2b0b5a143dc7c78454ec3bab9e3f6
Livenow14 <xowjd41@naver.com>
2020-09-15 16:01:43 +0900

    feat/ swagger config for global Authorize

--- src/main/java/org/greenbyme/angelhack/config/SwaggerConfig.java
+++ src/main/java/org/greenbyme/angelhack/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package org.greenbyme.angelhack.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,7 +12,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collection;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -23,11 +26,13 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("org.greenbyme.angelhack"))
                 .paths(PathSelectors.ant("/api/**"))
                 .build()
-                .apiInfo(metaData());
+                .apiInfo(metaData())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
     }
 
     private ApiInfo metaData() {
-      return new ApiInfoBuilder()
+        return new ApiInfoBuilder()
                 .title("GreenByMe REST API")
                 .description("Green by me, Green by earth(us)")
                 .version("0.4.0")
@@ -36,6 +41,28 @@ public class SwaggerConfig {
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .build();
-
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "jwt", "header");
+    }
+
+    private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+
 }
