32590ca12247d3131172e89a106a206c044a20c7
PandaHun <gnsgnsek@gmail.com>
2020-09-29 23:58:23 +0900

    refactor: Refactor Convention

--- src/main/java/org/greenbyme/angelhack/domain/posttag/PostTagRepository.java
+++ src/main/java/org/greenbyme/angelhack/domain/posttag/PostTagRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PostTagRepository extends JpaRepository<PostTag, Long> {
+
     Optional<PostTag> findByPostAndTag(Post post, Tag tag);
 
     List<PostTag> findAllByTag(Tag tag);
--- src/main/java/org/greenbyme/angelhack/domain/tag/TagRepository.java
+++ src/main/java/org/greenbyme/angelhack/domain/tag/TagRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
     Optional<Tag> findByTagName(String tagName);
 }
