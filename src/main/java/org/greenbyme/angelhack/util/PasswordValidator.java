3f706394b40d007f3c9dae0233b99c8a1e51dc8b
TaeJeong Kim <xowjd41@naver.com>
2021-01-15 14:33:55 +0900

    feat: Password 애노테이션 정의
    애노태이션으로 password를 valid할 수 있게 정의함.

---
+++ src/main/java/org/greenbyme/angelhack/util/PasswordValidator.java
@@ -0,0 +1,31 @@
+package org.greenbyme.angelhack.util;
+
+import org.greenbyme.angelhack.domain.user.Password;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.text.MessageFormat;
+
+public class PasswordValidator implements ConstraintValidator<Password, String> {
+
+    private static final int MIN_SIZE = 8;
+    private static final int MAX_SIZE = 50;
+    private static final String regexPassword = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{" + MIN_SIZE
+            + "," + MAX_SIZE + "}$";
+
+    @Override
+    public void initialize(Password constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        boolean isValidPassword = password.matches(regexPassword);
+        if (!isValidPassword) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(
+                    MessageFormat.format("{0}자 이상의 {1}자 이하의 숫자, 영문자, 특수문자를 포함한 비밀번호를 입력해주세요", MIN_SIZE, MAX_SIZE))
+                    .addConstraintViolation();
+        }
+        return isValidPassword;
+    }
+}
